b9619b3e973045921f168564e0604b83a53aa3fb
Barry07 <horgan.barry@gmail.com>
2015-02-08 14:41:25 +1100

    created userlist.jade made edits to app & index

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//new code
+var mongo = require ('mongodb');
+var monk = require ('monk');
+var db = monk ('localhost:27017/nodetest1');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,6 +26,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+//make db accessible to router
+app.use(function(req,res,next){
+	req.db = db;
+	next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
